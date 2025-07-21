191365f3bb9729b69fde56a54316a882c436f95f
Paula Mallol <paula@checklyhq.com>
2025-07-21 12:16:40 +0200

    feat: add repo to write in

--- utils/test.ts
+++ utils/test.ts
@@ -1,10 +1,9 @@
 import { codingAgent } from "./agent";
 import dotenv from "dotenv";
-
 dotenv.config({ path: ".env.local" });
-
 codingAgent(
-  "Add a contributing section to the readme of this project. Use standard format.", 
+  "Add a contributing section to the readme of this project. Use standard format.",
+  "https://github.com/pmallol/rakun-agent",
 )
   .then(console.log)
   .catch(console.error);
